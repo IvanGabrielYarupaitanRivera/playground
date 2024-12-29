08df1b2b0e806e46a18ac670c83bd6647db09712
Vanchi <ivangyr321@gmail.com>
2024-12-28 23:16:22 -0500

    agrega un nuevo componente Button en Svelte con contador de clics

---
+++ src/routes/basic-svelte/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let count = $state(0);
+
+	let { name }: { name: string } = $props();
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button onclick={handleClick} class="mt-4 rounded bg-red-500 px-2 py-1">
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'} by {name}
+</button>
