a8c4462d871f92080cb28f27295b66d4f6c5b968
Vanchi <ivangyr321@gmail.com>
2024-12-29 12:56:09 -0500

    agrega la página de Svelte Básico con el componente MensajePersonalizado y una imagen dinámica

---
+++ src/routes/basic-svelte/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import MensajePersonalizado from './MensajePersonalizado.svelte';
+
+	let source: string =
+		'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUEFqSVnXsXqQT_qIumH4c2uDciSgHK1PRVA&s';
+	let alternativeText: string = 'imagen de la portada del manga Dungeon ni Deai';
+</script>
+
+<!-- Viendo nuestro componente que hemos creado -->
+<MensajePersonalizado idComponente={1}></MensajePersonalizado>
+<MensajePersonalizado idComponente={2}></MensajePersonalizado>
+
+<!-- Agregamos una imagen con atributos dinámicos -->
+<img src={source} alt={alternativeText} />
