da968e4eb9cf0f55e745a619963913b3dd30b285
Vanchi <ivangyr321@gmail.com>
2024-12-29 21:27:29 -0500

    actualiza la página de Svelte Básico para mostrar un número aleatorio y elimina la selección de colores

--- src/routes/basic-svelte/+page.svelte
+++ src/routes/basic-svelte/+page.svelte
@@ -1,27 +1,21 @@
-<script>
-	const colors = ['red', 'green', 'blue'];
-	let selected = $state(colors[0]);
+<script lang="ts">
+	import { randomNumber } from './utils';
+
+	let number = $state(randomNumber());
+
+	function handleClick() {
+		number = randomNumber();
+	}
+
+	$inspect(number);
 </script>
 
-<h1
-	class="text-2xl"
-	class:text-red-500={selected === 'red'}
-	class:text-green-500={selected === 'green'}
-	class:text-blue-500={selected === 'blue'}
->
-	Pick a colour
-</h1>
+{#await number}
+	<p>...Esperando</p>
+{:then number}
+	<p>Número {number}</p>
+{:catch error}
+	<p>{error.message}</p>
+{/await}
 
-<div class="space-x-2">
-	{#each colors as color, i}
-		<button
-			onclick={() => (selected = color)}
-			class="p-4"
-			class:bg-red-500={color === 'red'}
-			class:bg-green-500={color === 'green'}
-			class:bg-blue-500={color === 'blue'}
-		>
-			{i}
-		</button>
-	{/each}
-</div>
+<button onclick={handleClick} class="bg-red-700">Número Aleatorio </button>
