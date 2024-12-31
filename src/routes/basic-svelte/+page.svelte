4cb16004be3d366ce0565474ca863d0524374f16
Vanchi <ivangyr321@gmail.com>
2024-12-31 02:25:48 -0500

    mejora el formulario de preguntas y respuestas en Svelte para incluir identificadores y mejorar la interacción del usuario

--- src/routes/basic-svelte/+page.svelte
+++ src/routes/basic-svelte/+page.svelte
@@ -1,44 +1,109 @@
 <script lang="ts">
-	let pregunta = '';
-	let respuesta = '';
+	type Pregunta = {
+		id: string;
+		pregunta: string;
+	};
+
+	let pregunta: Pregunta = $state({ id: '', pregunta: '' });
+	let respuesta = $state('');
 	const preguntas = [
-		'¿Cuál es la capital de Francia?',
-		'¿Cuál es la capital de España?',
-		'¿Cuál es la capital de Italia?',
-		'¿Cuál es la capital de Portugal?',
-		'¿Cuál es la capital de Alemania?',
-		'¿Cuál es la capital de Rusia?',
-		'¿Cuál es la capital de China?',
-		'¿Cuál es la capital de Japón?',
-		'¿Cuál es la capital de Australia?',
-		'¿Cuál es la capital de Argentina?',
-		'¿Cuál es la capital de Brasil?',
-		'¿Cuál es la capital de Chile?',
-		'¿Cuál es la capital de Colombia?',
-		'¿Cuál es la capital de Ecuador?',
-		'¿Cuál es la capital de Perú?',
-		'¿Cuál es la capital de Venezuela?',
-		'¿Cuál es la capital de Estados Unidos?',
-		'¿Cuál es la capital de Canadá?',
-		'¿Cuál es la capital de México?',
-		'¿Cuál es la capital de Cuba?'
+		{
+			id: 'Pregunta 01',
+			pregunta: '¿Cuál es la capital de Francia?'
+		},
+		{
+			id: 'Pregunta 02',
+			pregunta: '¿Cuál es la capital de España?'
+		},
+		{
+			id: 'Pregunta 03',
+			pregunta: '¿Cuál es la capital de Italia?'
+		},
+		{
+			id: 'Pregunta 04',
+			pregunta: '¿Cuál es la capital de Portugal?'
+		},
+		{
+			id: 'Pregunta 05',
+			pregunta: '¿Cuál es la capital de Alemania?'
+		},
+		{
+			id: 'Pregunta 06',
+			pregunta: '¿Cuál es la capital de Rusia?'
+		},
+		{
+			id: 'Pregunta 07',
+			pregunta: '¿Cuál es la capital de China?'
+		},
+		{
+			id: 'Pregunta 08',
+			pregunta: '¿Cuál es la capital de Japón?'
+		},
+		{
+			id: 'Pregunta 09',
+			pregunta: '¿Cuál es la capital de Australia?'
+		},
+		{
+			id: 'Pregunta 10',
+			pregunta: '¿Cuál es la capital de Argentina?'
+		},
+		{
+			id: 'Pregunta 11',
+			pregunta: '¿Cuál es la capital de Brasil?'
+		},
+		{
+			id: 'Pregunta 12',
+			pregunta: '¿Cuál es la capital de Chile?'
+		},
+		{
+			id: 'Pregunta 13',
+			pregunta: '¿Cuál es la capital de Colombia?'
+		},
+		{
+			id: 'Pregunta 14',
+			pregunta: '¿Cuál es la capital de Ecuador?'
+		},
+		{
+			id: 'Pregunta 15',
+			pregunta: '¿Cuál es la capital de Perú?'
+		},
+		{
+			id: 'Pregunta 16',
+			pregunta: '¿Cuál es la capital de Venezuela?'
+		},
+		{
+			id: 'Pregunta 17',
+			pregunta: '¿Cuál es la capital de Estados Unidos?'
+		},
+		{
+			id: 'Pregunta 18',
+			pregunta: '¿Cuál es la capital de Canadá?'
+		},
+		{
+			id: 'Pregunta 19',
+			pregunta: '¿Cuál es la capital de México?'
+		},
+		{
+			id: 'Pregunta 20',
+			pregunta: '¿Cuál es la capital de Cuba?'
+		}
 	];
 
 	const enviarRespuesta = () => {
-		alert(`La respuesta a "${pregunta}" es: ${respuesta}`);
+		alert(`La respuesta a la ${pregunta.id}: ${pregunta.pregunta}  es ${respuesta}`);
 	};
 </script>
 
 <label for="preguntas">Por favor seleccione una pregunta y responda</label>
 <select id="preguntas" name="preguntas" bind:value={pregunta}>
 	{#each preguntas as pregunta}
-		<option value={pregunta}>{pregunta}</option>
+		<option value={pregunta}>{pregunta.pregunta}</option>
 	{/each}
 </select>
 
 <br />
 
 <label for="respuesta">Ingrese la respuesta</label>
-<input type="text" id="respuesta" name="respuesta" bind:value={respuesta} disabled={!pregunta} />
+<input type="text" id="respuesta" name="respuesta" bind:value={respuesta} />
 
-<button onclick={enviarRespuesta} disabled={!respuesta}>Enviar Respuesta</button>
+<button onclick={enviarRespuesta}>Enviar Respuesta</button>
