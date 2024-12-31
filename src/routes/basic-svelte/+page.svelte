bf95814e90586617b1c9f6f0b3e1532ba823fc36
Vanchi <ivangyr321@gmail.com>
2024-12-31 01:07:53 -0500

    elimina la lógica de navegación en la página básica de Svelte al integrar el componente Stepper

--- src/routes/basic-svelte/+page.svelte
+++ src/routes/basic-svelte/+page.svelte
@@ -1,15 +1,3 @@
 <script lang="ts">
 	import Stepper from './Stepper.svelte';
-
-	let value = $state(0);
-
-	const backSteps = () => {
-		value--;
-	};
-
-	const nextSteps = () => {
-		value++;
-	};
 </script>
-
-<Stepper {backSteps} {nextSteps} {value}></Stepper>
