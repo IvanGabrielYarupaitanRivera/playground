d09f1fa5c00d97281d7472b105335c05257b1248
Vanchi <ivangyr321@gmail.com>
2024-12-31 19:10:20 -0500

    agrega el componente SeleccionCantidad para elegir el número de bolas de helado

---
+++ src/lib/components/SeleccionCantidad.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let cantidadBolasSeleccionada: string;
+	export let opcionesCantidad: string[];
+</script>
+
+<h2>Elige el número de bolas de helado</h2>
+{#each opcionesCantidad as cantidad}
+	<label for={cantidad}>
+		bola {cantidad}
+		<input
+			type="radio"
+			name="bolas"
+			id={cantidad}
+			bind:group={cantidadBolasSeleccionada}
+			value={cantidad}
+		/>
+	</label>
+{/each}
