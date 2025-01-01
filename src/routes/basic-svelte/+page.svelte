0a95fe6470aed90b821787acc3c571ffa397b926
Vanchi <ivangyr321@gmail.com>
2025-01-01 01:24:10 -0500

    agrega controles deslizantes para ajustar altura y ancho, y muestra el área calculada

---
+++ src/routes/basic-svelte/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	let altura = $state(10);
+	let ancho = $state(10);
+	let area = $derived(altura * ancho);
+</script>
+
+<input type="range" name="altura" id="altura" bind:value={altura} min="10" max="500" step="1" />
+<input type="range" name="ancho" id="ancho" bind:value={ancho} min="10" max="500" step="1" />
+
+<div class="bg-gray-100 p-4" style="height: {altura}px; width: {ancho}px;">{area}</div>
+
+<p>Altura: {altura}</p>
+<p>Ancho: {ancho}</p>
