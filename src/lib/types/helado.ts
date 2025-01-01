68c2eb616672eb5540f56f68e37735b21c97e5b4
Vanchi <ivangyr321@gmail.com>
2024-12-31 19:10:26 -0500

    agrega la interfaz ConfiguracionHelado y su configuración para opciones de cantidad y sabores de helado

---
+++ src/lib/types/helado.ts
@@ -0,0 +1,9 @@
+export interface ConfiguracionHelado {
+	opcionesCantidad: string[];
+	opcionesSabores: string[];
+}
+
+export const configuracionHelado: ConfiguracionHelado = {
+	opcionesCantidad: ['1', '2', '3', '4', '5'],
+	opcionesSabores: ['fresa', 'chocolate', 'vainilla', 'menta', 'mango']
+};
